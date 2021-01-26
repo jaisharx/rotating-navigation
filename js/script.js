3ad87da15f9891aa77729fae01025887bc8b6c98
jai-sharma <sendtojsharma@gmail.com>
2021-01-26 19:02:36 +0530

    added logic for showing/hiding nav

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-// type your js logic here...
+const open = document.getElementById('open');
+const close = document.getElementById('close');
+const container = document.querySelector('.container');
+
+open.addEventListener('click', () => {
+    container.classList.add('show-nav');
+});
+
+close.addEventListener('click', () => {
+    container.classList.remove('show-nav');
+});
